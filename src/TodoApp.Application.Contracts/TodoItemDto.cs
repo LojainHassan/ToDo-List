0614bc5073011153f494fffc5d92de3d79fe541c
unknown <lojainhteabat@gmail.com>
2023-06-12 00:17:10 +0300

    First Step and Initialization

---
+++ src/TodoApp.Application.Contracts/TodoItemDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoApp
+{
+    public class TodoItemDto
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+    }
+}
